9bdbb699aba9ef1e4ca424161c75b908dc495fff
s <s.looi2@outlook.com>
2024-06-23 20:48:11 +0900

    :sparkles: add argument to run extension server

--- make.sh
+++ make.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-if [ "$1" = "c" ]; then
+if [ "$1" = "ec" ]; then
     npm run --prefix apps/extension chokidar_content_scripts
-elif [ "$1" = "n" ]; then
+elif [ "$1" = "en" ]; then
     npm run --prefix apps/extension chokidar_normal
-else
+elif [ "$1" = "e" ]; then
     npm run --prefix apps/extension chokidar
+elif [ "$1" = "s" ]; then
+    npm run --prefix apps/extensionServer dev
 fi
