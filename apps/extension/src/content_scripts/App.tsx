b5d53a3b205d68879f476125611d0e3a3ab5b184
s <s.looi2@outlook.com>
2024-06-23 22:42:39 +0900

    Render button for each image

--- apps/extension/src/content_scripts/App.tsx
+++ apps/extension/src/content_scripts/App.tsx
@@ -1,10 +1,21 @@
 import Button from "./Button"
+import { useEffect, useState } from "react"
 
 export default function App() {
 	console.log("content_scripts.ts app ran")
+	const [images, setImages] = useState([...document.querySelectorAll("img")])
+
+	useEffect(() => {
+		const interval = setInterval(() => setImages([...document.querySelectorAll("img")]), 333)
+		return () => clearTimeout(interval)
+	}, [])
+
+
 	return (
 		<>
-			<Button />
+			{
+				images.map((image, i) => <Button key={i} />)
+			}
 		</>
 	)
 }
