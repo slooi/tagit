069ca627e79067b990a938049ce0ada5135b6b98
s <s.looi2@outlook.com>
2024-06-23 22:18:40 +0900

    :sparkles: make dummy post request when receive communicator message

--- apps/extension/src/background/background.ts
+++ apps/extension/src/background/background.ts
@@ -1,3 +1,9 @@
 import Communicator from "../shared/Communicator";
 
-Communicator.onMessage(payload => console.log("payload", payload))
+Communicator.onMessage(payload => {
+	// console.log("payload", payload)
+	fetch("http://localhost:8085/save/attached-media", {
+		method: "POST"
+	})
+})
+
