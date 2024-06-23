9d7edd5a10462bbc12d30d97a0dafb33d2d53453
s <s.looi2@outlook.com>
2024-06-23 22:45:06 +0900

    :sparkles: make code use callback as originally inteded

--- apps/extension/src/background/background.ts
+++ apps/extension/src/background/background.ts
@@ -1,7 +1,7 @@
 import Communicator from "../shared/Communicator";
 
 Communicator.onMessage(payload => {
-	// console.log("payload", payload)
+	console.log("payload", payload)
 	fetch("http://localhost:8085/save/attached-media", {
 		method: "POST"
 	})
--- apps/extension/src/shared/Communicator.ts
+++ apps/extension/src/shared/Communicator.ts
@@ -2,9 +2,7 @@ type Payload = number
 
 export default abstract class Communicator {
 	public static onMessage(cb: (payload: Payload, sender: browser.runtime.MessageSender, sendResponse: (response?: any) => void) => boolean | void | Promise<any>) {
-		browser.runtime.onMessage.addListener(() => {
-			console.log("got hit!", cb)
-		})
+		browser.runtime.onMessage.addListener(cb)
 	}
 	public static sendMessage(payload: Payload) {
 		browser.runtime.sendMessage(payload)
