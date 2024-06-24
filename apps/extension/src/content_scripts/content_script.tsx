6aa8e055ef10dd11a658fc338159092b4c5af4f6
s <s.looi2@outlook.com>
2024-06-25 02:31:02 +0900

    :bug: fix twitter not working due to same ID

--- apps/extension/src/content_scripts/content_script.tsx
+++ apps/extension/src/content_scripts/content_script.tsx
@@ -2,20 +2,21 @@ import { createRoot } from 'react-dom/client';
 import App from './App'
 const body = document.querySelector('body')
 const reactRoot = document.createElement('div')
-reactRoot.id = 'react-root'
+reactRoot.id = 'react-root2'
 
 // Styles
 reactRoot.style.position = "absolute"
 reactRoot.style.top = "0px"
 reactRoot.style.left = "0px"
 reactRoot.style.zIndex = "10000000"
+reactRoot.style.width = "100%"
 
 reactRoot.style.pointerEvents = "none"
 
 if (!body) throw new Error("ERROR: body does not exist!")
 body.prepend(reactRoot)
 
-const container = document.getElementById('react-root');
+const container = document.getElementById('react-root2');
 const root = createRoot(container!);
 
 root.render(<App />)  // Render react component
