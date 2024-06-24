18fe122e5c1c701d54f6a9165c92577694e3799a
s <s.looi2@outlook.com>
2024-06-24 17:31:19 +0900

    :art: rename app to reactRoot

--- apps/extension/src/content_scripts/content_script.tsx
+++ apps/extension/src/content_scripts/content_script.tsx
@@ -1,19 +1,19 @@
 import { createRoot } from 'react-dom/client';
 import App from './App'
 const body = document.querySelector('body')
-const app = document.createElement('div')
-app.id = 'react-root'
+const reactRoot = document.createElement('div')
+reactRoot.id = 'react-root'
 
 // Styles
-app.style.position = "absolute"
-app.style.top = "0px"
-app.style.left = "0px"
-app.style.zIndex = "10000000"
+reactRoot.style.position = "absolute"
+reactRoot.style.top = "0px"
+reactRoot.style.left = "0px"
+reactRoot.style.zIndex = "10000000"
 
-app.style.pointerEvents = "none"
+reactRoot.style.pointerEvents = "none"
 
 if (!body) throw new Error("ERROR: body does not exist!")
-body.prepend(app)
+body.prepend(reactRoot)
 
 const container = document.getElementById('react-root');
 const root = createRoot(container!);
