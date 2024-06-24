3134fa7719bb7ea4b3e561d2582275c9cc87ef13
s <s.looi2@outlook.com>
2024-06-24 17:24:34 +0900

    :sparkles: properly initialized images

--- apps/extension/src/content_scripts/App.tsx
+++ apps/extension/src/content_scripts/App.tsx
@@ -9,13 +9,16 @@ const SMALLEST_MEDIA_DIMENSION = 80
 const LARGEST_MEDIA_DIMENSION = 150
 
 export default function App() {
+	const [images, setImages] = useState<MediaElement[]>(getMediaElements())
+
 	console.log("content_scripts.ts app ran")
-	const [images, setImages] = useState<MediaElement[]>([...document.querySelectorAll("img")])
+
+	function getMediaElements() {
+		return [...document.querySelectorAll("img,video")].filter(el => (el.clientWidth >= LARGEST_MEDIA_DIMENSION && el.clientHeight >= SMALLEST_MEDIA_DIMENSION) || (el.clientWidth >= SMALLEST_MEDIA_DIMENSION && el.clientHeight >= LARGEST_MEDIA_DIMENSION)) as MediaElement[]
+	}
 
 	useEffect(() => {
-		const interval = setInterval(() => setImages(
-			[...document.querySelectorAll("img,video")].filter(el => (el.clientWidth >= LARGEST_MEDIA_DIMENSION && el.clientHeight >= SMALLEST_MEDIA_DIMENSION) || (el.clientWidth >= SMALLEST_MEDIA_DIMENSION && el.clientHeight >= LARGEST_MEDIA_DIMENSION)) as MediaElement[]
-		), 333)
+		const interval = setInterval(() => setImages(getMediaElements()), 333)
 		return () => clearTimeout(interval)
 	}, [])
 
