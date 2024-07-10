c507f7a34d04fa9042a21d66c303d7fb92037bf2
s <s.looi2@outlook.com>
2024-07-11 02:11:51 +0900

    :lipstick: change btn backgroundcolor if twitter is in dark mode

--- packages/extension/src/content_scripts/DownloadButton.tsx
+++ packages/extension/src/content_scripts/DownloadButton.tsx
@@ -12,12 +12,12 @@ export default function DownloadButton({ text, callback, mediaElement }: ButtonP
 		console.log("Hello button clicked!")
 		callback()
 	}
-
+	console.log("isDarkMode()", isDarkMode())
 	return (
 		<>
 			<button style={{
 				border: mediaElement instanceof HTMLVideoElement ? "2px dashed rgba(0,0,0,0.6)" : "1px solid black",
-				backgroundColor: mediaElement.src.startsWith("blob:") ? ORANGE_ALPHA : "rgba(200,200,200,0.7)",
+				backgroundColor: mediaElement.src.startsWith("blob:") ? ORANGE_ALPHA : isDarkMode() ? "rgba(110,110,110,0.7)" : "rgba(200,200,200,0.7)",
 				margin: "0px",
 				padding: "0px",
 				fontSize: "14px",
@@ -28,4 +28,10 @@ export default function DownloadButton({ text, callback, mediaElement }: ButtonP
 			</button>
 		</>
 	)
+}
+
+
+function isDarkMode() {
+	const element = document?.head?.querySelector("[name~=theme-color][content]")
+	return element?.getAttribute("content") === "#000000" // ts-ignore
 }
