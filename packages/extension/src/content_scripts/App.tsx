28370d5c3548832db89bc6203b0a92983b9fa9b2
s <s.looi2@outlook.com>
2024-07-10 11:33:44 +0900

    :sparkles: add press alt to hide panels + only show largest media if twitter overlay is active. EDIT LATER

--- packages/extension/src/content_scripts/App.tsx
+++ packages/extension/src/content_scripts/App.tsx
@@ -10,6 +10,12 @@ const LARGEST_MEDIA_DIMENSION = 150
 
 export default function App() {
 	const [mediaElements, setMediaElement] = useState<MediaElement[]>(getMediaElements())
+	const [showPanels, setShowPanels] = useState(true)
+	const keyHandler = (e: KeyboardEvent, isDown: boolean) => {
+		if (e.key === "Alt" && isDown) setShowPanels(!showPanels)
+	}
+	window.addEventListener("keydown", e => keyHandler(e, true))
+	window.addEventListener("keyup", e => keyHandler(e, false))
 
 	function getMediaElements() {
 		return [...document.querySelectorAll("img,video")].filter(el => {
@@ -25,5 +31,37 @@ export default function App() {
 		return () => clearTimeout(interval)
 	}, [])
 
-	return (<>{mediaElements.map((mediaElement, i) => <Panel key={i} mediaElement={mediaElement} />)}</>)
+	const hasTwitterOverlay = document.querySelectorAll("[data-testid=mask]")[0]
+	return (<>
+		{
+			showPanels ? (
+
+				hasTwitterOverlay ? (
+					filterForLargestImageOnScreen(mediaElements) && <Panel mediaElement={filterForLargestImageOnScreen(mediaElements)} />
+				) : (
+					mediaElements.map((mediaElement, i) => <Panel key={i} mediaElement={mediaElement} />)
+				)
+			) : <></>
+		}
+	</>)
+}
+
+function filterForLargestImageOnScreen<T extends MediaElement>(mediaElements: T[]): T {
+	let largestArea = 0
+	let largestMediaElement = mediaElements[0]
+	mediaElements.forEach(mediaElement => {
+		const area = mediaElement.clientHeight * mediaElement.clientWidth
+		const rect = mediaElement.getBoundingClientRect()
+		if (area > largestArea) {
+			if (boxCollision(0, 0, innerWidth, innerHeight, rect.x, rect.y, rect.width, rect.height)) {
+				largestMediaElement = mediaElement
+				largestArea = area
+			}
+		}
+	})
+	return largestMediaElement
+}
+
+function boxCollision(x0: number, y0: number, w0: number, h0: number, x1: number, y1: number, w1: number, h1: number) {
+	return (x0 + 30 < x1 + w1 && y0 + 30 < y1 + h1 && x0 + w0 - 30 > x1 && y0 + h0 - 30 > y1) ? true : false
 }
